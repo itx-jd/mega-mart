a824d09367a9fdabf52237a8b3f2017d2fa6c8ef
Talha Bin Yousaf <136476133+talha9445@users.noreply.github.com>
2024-05-03 20:51:59 +0500

    adding proxy server

---
+++ backend/proxy.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const app = express(); // Proxy middleware
+const productsProxy = createProxyMiddleware({
+  target: "http://localhost:5000",
+  changeOrigin: true,
+  pathRewrite: { "^/api/products": "/api/products" },
+}); // Proxy routes
+app.use("/api/products", productsProxy); // Start the proxy server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
+});
