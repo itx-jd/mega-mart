18cf1773d5daab8802583a74fc642c5bd375a7a4
Mustafa Mansoor <mustafamansoor47@gmail.com>
2024-05-08 07:49:50 +0500

    updated app.js to handle all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 // App.js
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,useLocation } from 'react-router-dom';
 import Navbar from "./Components/Navbar/nav";
 import Items from "./Components/Items/index";
 import Product from "./Components/Items/Product/product";
@@ -11,17 +11,77 @@ import LoginForm from './Components/LoginForm';
 
 
 function InnerApp({ isAuthenticated, setIsAuthenticated }) {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const location = useLocation();
+  const [searchItem, SetSearchItem] = useState("");
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
+  const addToCart = (item) => {
+    setCartItems((prevItems) => {
+      const existingItem = prevItems.find((prevItem) => prevItem.id === item.id);
+
+      if (existingItem) {
+        return prevItems.map((prevItem) =>
+          prevItem.id === item.id
+            ? { ...prevItem, quantity: prevItem.quantity + item.quantity }
+            : prevItem
+        );
+      } else {
+        return [...prevItems, item];
+      }
+    });
+  };
+
+  const updateCart = (item) => {
+    setCartItems((prevItems) => {
+      const existingItem = prevItems.find((prevItem) => prevItem.id === item.id);
+  
+      if (existingItem) {
+        return prevItems.map((prevItem) =>
+          prevItem.id === item.id
+            ? { ...prevItem, quantity: item.quantity }
+            : prevItem
+        );
+      } else {
+        return [...prevItems, item];
+      }
+    });
+  }
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prevItems) => {
+      const updatedItems = prevItems.filter((prevItem) => {
+        return prevItem.id !== itemId;
+      });
+      return updatedItems;
+    });
+  };
+  
+  const removeAllFromCart = () => {
+    setCartItems([]);
+  };
+
+  useEffect(() => {
+    setIsCartOpen(false);
+  }, [location]);
+
   return (
     <>
-      {isAuthenticated && <Navbar />}
+      {isAuthenticated && <Navbar toggleCart={toggleCart} SearchItem={SetSearchItem}/>}
       <div style={{ marginTop: "100px" }}>
         <Routes>
-          <Route path="/" element={isAuthenticated ? <Items /> : <LoginForm setIsAuthenticated={setIsAuthenticated} />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/checkout/:price" element={<Checkout />} />
+          <Route path="/" element={isAuthenticated ? <Items SearchItem={searchItem}/> : <LoginForm setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/product/:productId" element={<Product addToCart={addToCart} />} />
+          <Route path="/checkout/:price" element={<Checkout cartItems={cartItems} removeAllFromCart={removeAllFromCart} />} />
         </Routes>
       </div>
-      {isAuthenticated && <Cart />}
+      {isAuthenticated && 
+      <Cart isOpen={isCartOpen} toggleCart={toggleCart} items={cartItems} removeFromCart={removeFromCart} updateCartQuantity={updateCart} />
+      }
     </>
   );
 }
